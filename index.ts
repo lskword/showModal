6f648fa5c925289013c2e54a57a2873b816bc7bd
lskword <532016205@qq.com>
2023-12-12 11:41:32 +0800

    v3.0

--- index.ts
+++ index.ts
@@ -1,16 +1,27 @@
 import {h, render} from "vue";
+enum modelType {
+    title = '提示',
+    isShowCancel = 1,
+    cancelColor = '#000',
+    confirmColor = '#527486',
+    confirmText = '确认',
+    cancelText = '取消',
+    content = '请输入内容'
+}
 
 interface IModal {
     title?: string
     content: string
-    isShowCancel?: boolean
+    isShowCancel?: boolean | number
     confirmText?: string
+    confirmColor?: string
     cancelText?: string
+    cancelColor?: string
     onSuccess?: () => void
     onCancel?: () => void
 }
 
-export function showModal({title = '提示', content, isShowCancel = true, confirmText = '确认', cancelText = '取消', onSuccess, onCancel}: IModal) {
+export function showModal({title = modelType.title, content = modelType.content, isShowCancel = modelType.isShowCancel, confirmText = modelType.confirmText, confirmColor = modelType.confirmColor, cancelText = modelType.cancelText, cancelColor = modelType.cancelColor, onSuccess, onCancel}: IModal ) {
     const nameId: string = ('my_phone_model_' + Math.random()).replace('.', '')
     const children = [
         h('div', {class: 'title'}, title),
@@ -24,17 +35,21 @@ export function showModal({title = '提示', content, isShowCancel = true, confi
     }
 
     let btns: any = [h('div', {
-        class: 'btn success', onClick: () => {
-            if (onSuccess) { onSuccess() }
+        class: 'btn success',
+        style: {color: confirmColor},
+        onClick: () => {
             close()
+            if (onSuccess) { onSuccess() }
         }
     }, {default: () => confirmText})]
     // 是否添加取消按钮
     if (isShowCancel) {
         btns.unshift(h('div', {
-            class: 'btn cancel', onClick: () => {
-                if (onCancel) { onCancel() }
+            class: 'btn cancel',
+            style: {color: cancelColor},
+            onClick: () => {
                 close()
+                if (onCancel) { onCancel() }
             }
         }, {default: () => cancelText}))
     }
